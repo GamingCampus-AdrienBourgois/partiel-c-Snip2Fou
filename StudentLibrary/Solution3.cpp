c777af7e47f76078e6b6f4566c5a665e4a6c354f
unknown <echoquet@gamingcampus.local>
2024-01-19 14:51:18 +0100

    ex3

--- StudentLibrary/Solution3.cpp
+++ StudentLibrary/Solution3.cpp
@@ -1,3 +1,4 @@
+
 #include "Solution3.h"
 
 // Don't fortget to enable the exercise in the SudentConfiguration.h file !
@@ -9,7 +10,11 @@ void Solution3::SetWords(const std::vector<std::string>& _words)
 	words = _words;
 }
 
-void Solution3::SortWords() {}
+void Solution3::SortWords() {
+	std::vector<std::string> new_words;
+	new_words = words;
+	std::sort(words.begin(), words.end());
+}
 
 std::vector<std::string> Solution3::GetSortedWords() const
 {
--- StudentLibrary/Solution3.h
+++ StudentLibrary/Solution3.h
@@ -1,6 +1,7 @@
 #pragma once
-
+#include <iostream>
 #include <string>
+#include <algorithm> 
 #include <vector>
 
 #include "StudentConfiguration.h"
