af10b101fa637de371913e38caac83e334df49d4
unknown <echoquet@gamingcampus.local>
2024-01-19 14:29:39 +0100

    ex2

--- StudentLibrary/Solution2.cpp
+++ StudentLibrary/Solution2.cpp
@@ -2,13 +2,35 @@
 
 #include <ostream>
 
+#include <fstream>
+
 // Don't forget to enable the exercise in the SudentConfiguration.h file !
 #include "StudentConfiguration.h"
 #ifdef COMPILE_EXERCICE_2
 
 float Solution2::GetBalance(const std::string& accountName)
 {
-	return -1.0f;
+	float solde = 0;
+	std::string line;
+	std::string action = "";
+	std::string value = "";
+
+	std::ifstream myfile(accountName + ".txt");
+	if (myfile.is_open()) {
+		std::string ligne;
+		std::string delimiter = " ";
+        while (std::getline(myfile, ligne)) {
+            size_t position = 0;
+			position = ligne.find(delimiter);
+            action = ligne.substr(0, position);
+			value = ligne.substr(position + 1, std::string::npos);
+			if (action == "DEPOSIT") {
+				solde += std::stoi(value);
+			}else if(action == "WITHDRAW") {
+				solde -= std::stoi(value);
+			}
+        }
+	}
 }
 
 #endif
